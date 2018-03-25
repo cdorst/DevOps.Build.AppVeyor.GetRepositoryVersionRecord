5736ea9d51efc5af998acc19cac6f992b42baf37
Christopher Dorst <chris@dorst.email>
2018-03-25 16:51:17 -0500

    Update RepositoryVersionRecordGetter.cs

--- DevOps.Build.AppVeyor.GetRepositoryVersionRecord/RepositoryVersionRecordGetter.cs
+++ DevOps.Build.AppVeyor.GetRepositoryVersionRecord/RepositoryVersionRecordGetter.cs
@@ -1,3 +1,6 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
 using DevOps.Build.AppVeyor.AzureStorageTableVersionLedger;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Threading.Tasks;
@@ -11,11 +14,11 @@ namespace DevOps.Build.AppVeyor.GetRepositoryVersionRecord
         /// <summary>Returns the given repository's version record from the Azure Storage Table AppVeyor build ledger</summary>
         public static async Task<RepositoryVersionTable> GetRepositoryVersionRecordAsync(string accountName, string repositoryName)
         {
-var operation = TableOperation.Retrieve<RepositoryVersionTable>(accountName, repositoryName);
-var table = await GetTable();
-var result = await table.ExecuteAsync(operation);
-if (result?.Result == null) return null;
-return (RepositoryVersionTable)result.Result;;
+            var operation = TableOperation.Retrieve<RepositoryVersionTable>(accountName, repositoryName);
+            var table = await GetTable();
+            var result = await table.ExecuteAsync(operation);
+            if (result?.Result == null) return null;
+            return (RepositoryVersionTable)result.Result;;
         }
     }
 }
